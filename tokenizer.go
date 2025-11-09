e5686a24da2e40ca0813e745e1776cfe0108d202
Ryan W <rbrickproductions123@gmail.com>
2025-11-08 23:44:43 -0600

    read numerics and equality

--- tokenizer.go
+++ tokenizer.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"errors"
 	"io"
-	"log"
+	"math/big"
 	"strings"
+	"unicode"
 )
 
 // very simple charset
@@ -32,8 +33,10 @@ const (
 )
 
 type Token struct {
-	Type    TokenType
-	Literal string
+	Type      TokenType
+	Literal   string
+	Numeric   *big.Float
+	IsDecimal bool
 }
 
 type Tokenizer struct {
@@ -42,8 +45,6 @@ type Tokenizer struct {
 
 func (t *Tokenizer) ReadString() (string, error) {
 	var sb strings.Builder
-
-	log.Println("hello world")
 
 	for {
 		c, _, err := t.reader.ReadRune()
@@ -97,6 +98,111 @@ func (t *Tokenizer) ReadString() (string, error) {
 	return sb.String(), nil
 }
 
+func (t *Tokenizer) readEquality(op rune) (*Token, error) {
+	second, _, err := t.reader.ReadRune()
+
+	if err == io.EOF {
+		if op == '=' {
+			return nil, errors.New("incomplete operator")
+		}
+
+		return &Token{
+			Literal: string(op),
+			Type:    Operator,
+		}, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	// weird syntax like '=>' or '=<' or '=!'
+	if op == '=' && second != '=' {
+		return nil, errors.New("unsupported equality operation")
+	}
+
+	if op != '=' && second != '=' {
+		return &Token{
+			Literal: string(op),
+			Type:    Operator,
+		}, nil
+	}
+
+	// validate the third rune is not '=', ('===' or '>==', '<==') etc.
+	third, _, err := t.reader.ReadRune()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if third == '=' {
+		return nil, errors.New("unsupported equality operation")
+	}
+
+	return &Token{
+		Literal: string(op) + "=",
+		Type:    Operator,
+	}, nil
+}
+
+func (t *Tokenizer) readNumeric() (*Token, error) {
+	t.reader.UnreadRune()
+
+	var literal strings.Builder
+	var numeric strings.Builder
+
+	isDecimal := false
+
+	for {
+		c, _, err := t.reader.ReadRune()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		if c >= '0' && c <= '9' {
+			literal.WriteRune(c)
+			numeric.WriteRune(c)
+		} else if c == '_' || c == ',' {
+			// should support commas so we can do a >= 1,000,000,000
+			literal.WriteRune(c)
+		} else if c == '.' {
+			if isDecimal {
+				// error: already a decimal
+				return nil, errors.New("invalid numeric, already a decimal")
+			}
+
+			// mark as a decimal
+			isDecimal = true
+
+			literal.WriteRune(c)
+			numeric.WriteRune(c)
+		} else {
+			// unread
+			t.reader.UnreadRune()
+			break // break out of loop
+		}
+	}
+
+	f, ok := new(big.Float).SetString(numeric.String())
+
+	if !ok {
+		return nil, errors.New("failed to parse float")
+	}
+
+	return &Token{
+		Type:      Numeric,
+		Numeric:   f,
+		Literal:   literal.String(),
+		IsDecimal: isDecimal,
+	}, nil
+
+}
+
 func (t *Tokenizer) ReadToken() (*Token, error) {
 	r, _, err := t.reader.ReadRune()
 
@@ -112,6 +218,10 @@ func (t *Tokenizer) ReadToken() (*Token, error) {
 				Literal: string(r),
 			}, nil
 		}
+	case Equal, Exclamation, LessThan, GreaterThan:
+		{
+			return t.readEquality(r)
+		}
 	case Quote:
 		{
 			// read a string up to next quote
@@ -124,8 +234,22 @@ func (t *Tokenizer) ReadToken() (*Token, error) {
 				Literal: str,
 			}, nil
 		}
+
 	default:
 		{
+			// discard whitespace
+			if unicode.IsSpace(r) {
+				return nil, nil
+			}
+
+			// try reading a numeric
+			if r >= '0' && r <= '9' {
+				// number
+				return t.readNumeric()
+			}
+
+			// try reading an identifier
+
 			return &Token{}, nil
 		}
 	}
--- tokenizer_test.go
+++ tokenizer_test.go
@@ -1,6 +1,7 @@
 package yap
 
 import (
+	"math/big"
 	"strings"
 	"testing"
 )
@@ -21,4 +22,49 @@ func TestReadString(t *testing.T) {
 	}
 
 	t.Log("read string:", str, "expected string:", expect)
+}
+
+func TestReadGreaterThan(t *testing.T) {
+	test := `>`
+	tokenizer := NewTokenizer(strings.NewReader(test))
+
+	token, err := tokenizer.ReadToken()
+
+	if err != nil {
+		t.FailNow()
+	}
+
+	if token.Literal != ">" {
+		t.Fail()
+		t.Logf("failed to get greater than token: got %s, expected >", token.Literal)
+	}
+}
+
+func TestReadNumeric(t *testing.T) {
+	test := `10,000,000_000.314159 `
+	expect := `10,000,000_000.314159`
+	expectNumeric, _ := new(big.Float).SetString("10000000000.314159")
+	tokenizer := NewTokenizer(strings.NewReader(test))
+
+	token, err := tokenizer.ReadToken()
+
+	if err != nil {
+		t.FailNow()
+	}
+
+	if !token.IsDecimal {
+		t.Fail()
+		t.Logf("expected decimal, got false, result: %s", token.Literal)
+	}
+
+	if token.Literal != expect {
+		t.Fail()
+		t.Logf("got: %s, expected: %s", expect, token.Literal)
+	}
+
+	if token.Numeric.Cmp(expectNumeric) != 0 {
+		t.Fail()
+		t.Logf("expected: %s, got: %s", expectNumeric.String(), token.Numeric.String())
+	}
+
 }
