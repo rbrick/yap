24788539e5f722f7c38f3a7f95d8c3fa8f50a0b2
Ryan W <rbrickproductions123@gmail.com>
2025-11-18 15:09:13 -0600

    work on ast

--- ast.go
+++ ast.go
@@ -1,1 +1,17 @@
 package yap
+
+type Expr struct{}
+
+type BinOp struct {
+	Left     Expr
+	Operator string
+	Right    Expr
+}
+type FuncCall struct {
+	Name string
+	Args []Expr
+}
+
+type Literal[T any] struct {
+	Value T
+}
